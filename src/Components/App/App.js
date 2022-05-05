89b1f7ad45d7ff18095a6f9bb018afd19824117a
RobertHJones <roberthuwjones@outlook.com>
2022-05-05 14:51:50 +0100

    fixed date

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,8 +8,6 @@ function App() {
   const [cityName, setCityName] = useState("");
   const [cityData, setCityData] = useState([]);
 
- 
-
   async function fetchData(city) {
     const response = await fetch(
       `https://community-open-weather-map.p.rapidapi.com/forecast/daily?q=${city}%2Cus&lat=35&lon=139&cnt=8&units=metric%20or%20imperial`,
@@ -24,15 +22,25 @@ function App() {
     setCityName(data.city.name);
     setCityData(data.list);
     console.log(data);
+    console.log("date", data.list[0].dt);
   }
 
   return (
     <div className="App">
+      <h1>Your Weather</h1>
       <Input onSubmit={fetchData} />
       {cityName !== "" && <div>Search results for: {cityName}</div>}
-      <div>
+      <div className="weatherData">
         {cityData.map((data) => {
-          return <Card key={data.sunrise} date={data.dt} tempMax={data.temp.max} tempMin={data.temp.min} desc={data.weather[0].description}/>;
+          return (
+            <Card
+              key={data.sunrise}
+              date={data.dt}
+              tempMax={data.temp.max}
+              tempMin={data.temp.min}
+              desc={data.weather[0].description}
+            />
+          );
         })}
       </div>
     </div>
