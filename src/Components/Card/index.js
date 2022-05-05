082440e977f268651eb1abff86d947f1e82d5549
RobertHJones <roberthuwjones@outlook.com>
2022-05-05 14:58:14 +0100

    temp

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,11 +1,4 @@
 export default function Card({ date, tempMax, tempMin, desc }) {
-  // function toDateTime(date)
-  // {
-  //     const t = new Date(1970, 0, 1);
-  //     t.setSeconds(date);
-  //     return t;
-  // }
-
   const newDate = new Date(date * 1000);
   const realDate = newDate.toLocaleDateString();
   console.log(date, "is the date");
@@ -13,8 +6,8 @@ export default function Card({ date, tempMax, tempMin, desc }) {
   return (
     <div>
       <h3>{realDate}</h3>
-      <p>Maximum temperature of {tempMax.toFixed(0)} degrees kelvin</p>
-      <p> Minimum temperatire of {tempMin.toFixed(0)} degrees kelvin</p>
+      <p>Maximum temperature of {tempMax.toFixed(0) - 273} degrees celsius</p>
+      <p> Minimum temperatire of {tempMin.toFixed(0) - 273} degrees celsius</p>
       <p>The forecast is {desc}</p>
     </div>
   );
